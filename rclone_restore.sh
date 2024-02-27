2009e9ba0122059837f0e81a09d0ac074be7928b
lambda1103 <lukasheider.lh@gmail.com>
2024-02-27 10:27:00 +0100

    added selfdestroy script

--- rclone_restore.sh
+++ rclone_restore.sh
@@ -2,13 +2,13 @@
 
 export $(grep -v '^#' .env | xargs)
 
-latest=$(rclone lsf ${RCLONE_REMOTE}: --include ${BACKUP_NAME}-* --files-only --order-by 'modtime' -v | tail -n 1)
+latest=$(rclone lsf ${RCLONE_REMOTE}:${BACKUP_NAME} --include "world-*" --files-only --order-by 'modtime' -v | tail -n 1)
 
 if [ -z "$latest" ]; then
     echo "No backup found"
     exit 1
 else
     echo "Backup found: $latest"
-    rclone copy ${RCLONE_REMOTE}:${latest} /mc-backups
+    rclone copy ${RCLONE_REMOTE}:${latest} ./mc-backups
     exit 0
 fi
---
+++ selfdestroy.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+export $(grep -v '^#' .env | xargs)
+
+if [ -f /root/minecraft/mc-data/.paused ]; then
+# Find Server ID
+server_id=$(curl -s -X GET \
+    -H "Authorization: Bearer $HETZNER_API_TOKEN" \
+    -H "Content-Type: application/json" \
+    "https://api.hetzner.cloud/v1/servers" | \
+    jq -r --arg SERVER_NAME "$SERVER_NAME" '.servers[] | select(.name == $SERVER_NAME) | .id')
+
+# Check if server exists
+if [ -z "$server_id" ]; then
+    echo "Server $SERVER_NAME not found."
+     # Send error message to Discord webhook
+    curl -H "Content-Type: application/json" \
+         -X POST \
+         -d "{\"content\":\"Error: Server $SERVER_NAME not found for deletion.\"}" \
+         $DISCORD_WEBHOOK_URL
+    exit 1
+fi
+
+# Delete Server
+curl -X DELETE \
+    -H "Authorization: Bearer $HETZNER_API_TOKEN" \
+    -H "Content-Type: application/json" \
+    "https://api.hetzner.cloud/v1/servers/$server_id"
+
+echo "Server $SERVER_NAME with ID $server_id deleted successfully."
+
+fi
+
+# Check if deletion was successful
+if [ $? -eq 0 ]; then
+    echo "Server $SERVER_NAME with ID $server_id deleted successfully."
+    # Send success message to Discord webhook
+    curl -H "Content-Type: application/json" \
+         -X POST \
+         -d "{\"content\":\"Success: Server $SERVER_NAME with ID $server_id deleted successfully.\"}" \
+         $DISCORD_WEBHOOK_URL
+else
+    echo "Failed to delete server $SERVER_NAME with ID $server_id."
+    # Send error message to Discord webhook
+    curl -H "Content-Type: application/json" \
+         -X POST \
+         -d "{\"content\":\"Error: Failed to delete server $SERVER_NAME with ID $server_id.\"}" \
+         $DISCORD_WEBHOOK_URL
+    exit 1
+fi
